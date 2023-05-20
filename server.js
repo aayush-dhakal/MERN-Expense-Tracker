9a163faa23f4a1e95c5cfc5ab27d0eaa8351c772
aayush679 <aayush.d@homeloanexperts.com.au>
2023-05-20 17:52:19 +0545

    basic backend setup and initialized react frontend

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+const connectDb = require("./config/connectDb");
+
+dotenv.config();
+
+connectDb();
+
+const app = express();
+
+//middlewares
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(cors());
+
+//routes
+// app.use("/api/v1/users", require("./routes/userRoute"));
+app.get("/", (req, res) => {
+  res.send("<h1>Hi guys</h1>");
+});
+
+const PORT = 8080 || process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
