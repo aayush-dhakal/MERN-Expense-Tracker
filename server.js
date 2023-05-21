955f9a168be830f50b176c0fd339094a93eabf2e
aayush-dhakal <aayushdhakal77@gmail.com>
2023-05-21 20:46:45 +0545

    ready for deployment

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
 const connectDb = require("./config/connectDb");
-const moment = require("moment");
+const path = require("path");
 
 dotenv.config();
 
@@ -19,6 +19,14 @@ app.use(cors());
 //routes
 app.use("/api/v1/users", require("./routes/userRoute"));
 app.use("/api/v1/transactions", require("./routes/transactionRoutes"));
+
+// providing location for static file. By setting up this middleware, Express will automatically handle requests for static files (e.g., CSS, JavaScript, images) by looking for them in the specified directory and serving them to the client.
+app.use(express.static(path.join(__dirname, "./client/build")));
+
+// rendering the static index file created from react build
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 const PORT = 4000 || process.env.PORT;
 
